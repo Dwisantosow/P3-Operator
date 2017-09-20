432f2c7f8037a3228e480142df1359c6f52c2e25
Dwisantosow <32090298+Dwisantosow@users.noreply.github.com>
2017-09-20 19:36:51 +0700

    Update op.increment-decrement.java

--- op.increment-decrement.java
+++ op.increment-decrement.java
@@ -1,27 +1,27 @@
-pubic class DemoIncrementDecrement {
+public class DemoIncrementDecrement {
 
-	public static void main(String)[] args){
+	public static void main(String[] args) {
 		int x=1, y=1, z=1;
 
-		//Nilai Awal
-		System.out.printIn("nilai x : " +x);
-		System.out.printIn("nilai y : " +y);
-		System.out.printIn("nilai z : " +z);
+		//Nilai awal
+		System.out.println("Nilai x : " + x);
+		System.out.println("Nilai y : " + y);
+		System.out.println("Nilai z : " + z);
 
 		//Increment
 		x++; y++;
-		System.out.printIn("nilai x : " + x );
-		System.out.printIn("nilai y : " + y++ );
-		System.out.printIn("nilai z : " + ++z );
+		System.out.println("Nilai x : " + x);
+		System.out.println("Nilai y : " + y++);
+		System.out.println("Nilai z : " + ++z);
 
-		System.out.printIn("nilai y : " + y );
+		System.out.println("Nilai y : " + y);
 
 		//Decrement
 		x--; y--;
-		System.out.printIn("nilai x : " + x );
-		System.out.printIn("nilai y : " + y-- );
-		System.out.printIn("nilai z : " + --z );
+		System.out.println("Nilai x : " + x);
+		System.out.println("Nilai y : " + y--);
+		System.out.println("Nilai z : " + --z);
 
-		System.out.printIn("nilai y : " + y );
+		System.out.println("Nilai y : " + y);
 	}
 }
